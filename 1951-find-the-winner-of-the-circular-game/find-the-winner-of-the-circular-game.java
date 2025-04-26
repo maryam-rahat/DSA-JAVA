f9e1a36ffe5fb337711b502c9b2b6682674a3386
Maryam Rahat <maryamrahat0@gmail.com>
2025-04-26 23:59:00 +0530

    Time: 2 ms (59.08%) | Memory: 41.2 MB (33.39%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -1,17 +1,22 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        LinkedList<Integer> circle = new LinkedList<>();
+        ArrayList<Integer> res = new ArrayList<>();
 
-        for(int i = 1; i <=n; i++){
-            circle.add(i);
+        for(int i = 1; i<= n; i++){
+            res.add(i);
         }
 
-        int index = 0;
-
-        while(circle.size()>1){
-            index = (index + k -1) % circle.size();
-            circle.remove(index);
+        return solve(res, k, 0);
+    }
+    private int solve(ArrayList<Integer> res, int k, int index){
+        if(res.size()==1){
+            return res.get(0);
         }
-        return circle.get(0);
+
+        index = (index + k-1) % res.size();
+        res.remove(index);
+
+        return solve(res, k, index);
+        //induction is not there, so done
     }
 }
