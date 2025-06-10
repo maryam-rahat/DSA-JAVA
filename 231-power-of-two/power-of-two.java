ae451b704805a93c4aaf7305f95f75f0b211d829
Maryam Rahat <maryamrahat0@gmail.com>
2025-06-11 00:14:45 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (38.56%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n==0) return false;
-        if(n == -2147483648) return false;
+        if(n<=0) return false;
+
         return ((n&(n-1))==0);
     }
 }
