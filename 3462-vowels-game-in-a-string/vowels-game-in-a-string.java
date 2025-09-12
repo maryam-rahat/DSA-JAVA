e71e938bbc4bbdc430f8d257f23cc5c3b0df4c69
Maryam Rahat <maryamrahat0@gmail.com>
2025-09-12 21:43:28 +0530

    Time: 11 ms (34.12%) | Memory: 45.4 MB (92.65%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean doesAliceWin(String s) {
+        for (char c : s.toCharArray()) {
+            if ("aeiou".indexOf(c) != -1) {
+                return true; 
+            }
+        }
+        return false;
+
+
+    }
+}
