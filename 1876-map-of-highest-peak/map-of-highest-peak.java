c43bead26253df9e0f2ff2b6a536e0a4f44328ec
Maryam Rahat <maryamrahat0@gmail.com>
2026-01-17 12:53:30 +0530

    Time: 45 ms (58.78%) | Memory: 184.2 MB (41.74%) - LeetSync

--- 1876-map-of-highest-peak/map-of-highest-peak.java
+++ 1876-map-of-highest-peak/map-of-highest-peak.java
@@ -1,6 +1,7 @@
 class Solution {
     int m, n;
-    int[][] dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    int[][] dir = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+
     public int[][] highestPeak(int[][] isWater) {
         m = isWater.length;
         n = isWater[0].length;
@@ -8,34 +9,29 @@ class Solution {
         int[][] height = new int[m][n];
         Queue<int[]> q = new LinkedList<>();
 
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(isWater[i][j] == 1){
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isWater[i][j] == 1) {
                     height[i][j] = 0;
-                    q.offer(new int [] {i, j});
-                }
-                else{
+                    q.offer(new int[] { i, j });
+                } else {
                     height[i][j] = -1;
                 }
             }
         }
 
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i = 0; i < size; i++){
-                int[] cur = q.poll();
-                int r = cur[0];
-                int c = cur[1];
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int r = cur[0];
+            int c = cur[1];
 
-                for(int[]d : dir){
-                    int nr = r + d[0];
-                    int nc = c + d[1];
-                    if(nr >= 0 && nc >= 0 && nr < m && nc < n && height[nr][nc] == -1){
-                        height[nr][nc] = height[r][c] +1;
-                        q.offer(new int[] {nr, nc});
-                    }
+            for (int[] d : dir) {
+                int nr = r + d[0];
+                int nc = c + d[1];
+                if (nr >= 0 && nc >= 0 && nr < m && nc < n && height[nr][nc] == -1) {
+                    height[nr][nc] = height[r][c] + 1;
+                    q.offer(new int[] { nr, nc });
                 }
-
             }
         }
         return height;
