47fcf57b6efb087c985b8a29bfddac7123b4dc95
Maryam Rahat <maryamrahat0@gmail.com>
2026-01-17 13:27:39 +0530

    Time: 15 ms (62.88%) | Memory: 47.6 MB (49.76%) - LeetSync

--- 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.java
+++ 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.java
@@ -3,16 +3,16 @@ class Solution {
     int [][] dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     public int maxDistance(int[][] grid) {
         m = grid.length;
-        int[][]dist = new int[m][m];
+        // int[][]dist = new int[m][m];
         Queue<int[]> q = new LinkedList<>();
 
         for(int i = 0; i < m; i++){
             for(int j = 0; j < m; j++){
                 if(grid[i][j] == 1){
                     q.offer(new int[] {i, j});
-                    dist[i][j] = 0;
+                    grid[i][j] = 0;
                 }else{
-                    dist[i][j] = -1;
+                    grid[i][j] = -1;
                 }
             }
         }
@@ -26,9 +26,9 @@ class Solution {
                 int nr = r + d[0];
                 int nc = c + d[1];
 
-                if(nr >= 0 && nc >= 0 && nr < m && nc < m && dist[nr][nc] == -1){
-                    dist[nr][nc] = dist[r][c] + 1;
-                    max = Math.max(max, dist[nr][nc]);
+                if(nr >= 0 && nc >= 0 && nr < m && nc < m && grid[nr][nc] == -1){
+                    grid[nr][nc] = grid[r][c] + 1;
+                    max = Math.max(max, grid[nr][nc]);
                     q.offer(new int[]{nr, nc});
                 }
             }
