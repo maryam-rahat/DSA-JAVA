7e455d1a36f5baab65aac61bfef3c7d8cb6b0f2c
Maryam Rahat <maryamrahat0@gmail.com>
2026-01-24 16:01:15 +0530

    Time: 19 ms (98.83%) | Memory: 47.6 MB (88.70%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,43 +1,46 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String> set = new HashSet<>();
-        Queue<String> q = new LinkedList<>();
+        Set<String> wordSet = new HashSet<>(wordList);
+        if(!wordSet.contains(endWord)) return 0;
+        Set<String> beginSet = new HashSet<>();
+        Set<String> endSet = new HashSet<>();
 
-        for (String word : wordList) {
-            set.add(word);
-        }
-        if (!set.contains(endWord))
-            return 0;
-        q.offer(beginWord);
+        beginSet.add(beginWord);
+        endSet.add(endWord);
+
         int level = 1;
 
-        while (!q.isEmpty()) {
-            int size = q.size();
-            for (int s = 0; s < size; s++) {
-                String currentWord = q.poll();
-                if (currentWord.equals(endWord))
-                    return level;
-                char[] arr = currentWord.toCharArray();
+        while(!beginSet.isEmpty() && !endSet.isEmpty()){
+            if(beginSet.size()>endSet.size()){
+                Set<String> temp = beginSet;
+                beginSet = endSet;
+                endSet = temp;
+            }
 
-                for (int i = 0; i < arr.length; i++) {
+            Set<String> nextSet = new HashSet<>();
+
+            for(String word: beginSet){
+                char arr[] = word.toCharArray();
+
+                for(int i = 0; i < arr.length; i++){
                     char original = arr[i];
 
-                    for (char ch = 'a'; ch <= 'z'; ch++) {
-                        if (ch == original)
-                            continue;
-
+                    for(char ch = 'a'; ch <= 'z'; ch++){
+                        if(ch == original) continue;
                         arr[i] = ch;
                         String newWord = new String(arr);
 
-                        if (set.contains(newWord)) {
-                            q.offer(newWord);
-                            set.remove(newWord);
+                        if(endSet.contains(newWord)) return level + 1;
 
+                        if(wordSet.contains(newWord)){
+                            nextSet.add(newWord);
+                            wordSet.remove(newWord);
                         }
                     }
                     arr[i] = original;
                 }
             }
+            beginSet = nextSet;
             level++;
         }
         return 0;
